389dd4e3d2e0673f7b95ff32e97365926af6fead
Hugo <hugomadrid12@hotmail.com>
2019-12-04 19:20:45 -0600

    se agregaron los grupos de radiobotones

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,18 +1,30 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
+
+
 public class Controller {
+
+    @FXML
+    private void initialize() {
+
+    }
+
     public void abrir(ActionEvent actionEvent) {
 
         try
         {
             Stage planillaStage=new Stage();
-            AnchorPane root = (AnchorPane) FXMLLoader.load(getClass().getResource("registroCita.fxml"));
+            AnchorPane root = (AnchorPane) FXMLLoader.load(getClass().getResource("funcionesOrganicasGenerales.fxml"));
             Scene scene = new Scene(root,360,290);
 
             planillaStage.setScene(scene);
@@ -22,5 +34,9 @@ public class Controller {
         {
 
         }
+    }
+
+    public void seleccionApetito(ActionEvent actionEvent) {
+
     }
 }
