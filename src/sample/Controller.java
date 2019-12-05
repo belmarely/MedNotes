df43acb712afc09ac5cccdd33b068a9d54a305d1
Belkis <belkismarely98@gmail.com>
2019-12-05 17:09:06 -0600

    Primer commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,26 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class Controller {
+    public void abrir(ActionEvent actionEvent) {
+
+        try
+        {
+            Stage planillaStage=new Stage();
+            AnchorPane root = (AnchorPane) FXMLLoader.load(getClass().getResource("registroCita.fxml"));
+            Scene scene = new Scene(root,360,290);
+
+            planillaStage.setScene(scene);
+            planillaStage.show();
+        }
+        catch(Exception e)
+        {
+
+        }
+    }
+}
