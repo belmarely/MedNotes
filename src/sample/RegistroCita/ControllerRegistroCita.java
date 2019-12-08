a8044c84d54c0a9cf1460f485971790a0e42291c
Allan <allanmonje12@outlook.com>
2019-12-07 23:46:26 -0600

    Codificando el registro de citas

---
+++ src/sample/RegistroCita/ControllerRegistroCita.java
@@ -0,0 +1,86 @@
+package sample.RegistroCita;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+import sample.modelos.MisFunciones;
+import sample.modelos.RegistroCitas;
+
+public class ControllerRegistroCita {
+    private String fechaConcatenada;
+    @FXML
+    private TextField identidad;
+    @FXML
+    private TextField nombre;
+    @FXML
+    private TextField apellido;
+    @FXML
+    private TextField dia;
+    @FXML
+    private ComboBox comboId;
+    @FXML
+    private Button Cancelar;
+    @FXML
+    private ScrollBar scrolll;
+
+    @FXML
+    private void initialize() {
+      //  relacionesRadioBotones();
+    //    llenarCampos();
+    }
+
+    public void Cancelar() {
+        Stage stage = (Stage) Cancelar.getScene().getWindow();
+        stage.close();
+    }
+
+    private boolean comprobarCamposVacios(){
+
+        if (!identidad.getText().isBlank() &&
+                !nombre.getText().isBlank() &&
+                !apellido.getText().isBlank() &&
+                !dia.getText().isBlank() &&
+                !comboId.getItems().isEmpty()){
+            return true;
+        }
+        return false;
+    }
+
+    public RegistroCitas crearIntancia(){
+        RegistroCitas cita = new
+                RegistroCitas(fechaConcatenada);
+        return cita;
+    }
+
+    public void Guardar() {
+        if (comprobarCamposVacios()) {
+            RegistroCitas cita = crearIntancia();
+            RegistroCitas.Guardar(cita);
+            mensaje("Datos guardados exitosamente");
+            Cancelar();
+
+        }else{
+            mensaje("Campos Vacios");
+        }
+    }
+    public void BotonGuardarActualizar(ActionEvent actionEvent){
+        if (MisFunciones.getIdExamenFisico()!=0) {
+            System.out.println("Pronto actualizara");
+            //Actualizar();
+        } else {
+            Guardar();
+        }
+    }
+    private void mensaje(String mensaje){
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setTitle(null);
+        alert.setHeaderText(null);
+        alert.setContentText(mensaje);
+        alert.showAndWait();
+    }
+
+
+
+}
--- src/sample/modelos/RegistroCitas.java
+++ src/sample/modelos/RegistroCitas.java
@@ -1,4 +1,54 @@
 package sample.modelos;
 
+import sample.libs.Conexion;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Date;
+
 public class RegistroCitas {
+    private String identidad;
+    private String nombre;
+    private String apellido;
+    private String fechaCita;
+    private Date creacion;
+
+    public RegistroCitas(String fechaConcatenada) {
+        this.fechaCita = fechaConcatenada;
+
+    }
+
+    public String getFechaCita() {
+        return fechaCita;
+    }
+
+    public void setFechaCita(String fechaCita) {
+        this.fechaCita = fechaCita;
+    }
+
+    public static boolean Guardar(RegistroCitas cita){
+        try{
+            PreparedStatement sentencia = Conexion.abrirConexion().prepareStatement(
+                    "INSERT INTO `registro_medico`.citas`` (`id_expediente`" +
+                            ", `dia`) VALUES (?, ?);"
+            );
+            sentencia = cita.seteandoSentenciaPreparada(cita,sentencia);
+            boolean resultado=  sentencia.execute();
+            return resultado;
+        }catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return false;
+    }
+
+    private PreparedStatement seteandoSentenciaPreparada(RegistroCitas citacion, PreparedStatement sentencia){
+        try{
+            sentencia.setInt(1,MisFunciones.getIdPaciente());
+            sentencia.setString(2, citacion.getFechaCita());
+
+        }catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return sentencia;
+    }
 }
