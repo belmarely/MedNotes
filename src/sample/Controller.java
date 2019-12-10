7c1f9a51f1d7a8db159f49134d91766566664362
Belkis <belkismarely98@gmail.com>
2019-12-09 18:26:58 -0600

    login conectado con mysql

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -9,24 +9,8 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-import sample.libs.Conexion;
-
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 
 public class Controller {
-    @FXML
-    private Button btnLogin;
-
-    @FXML
-    private TextField txtUsuario;
-
-    @FXML
-    private PasswordField txtPassword;
-
-    @FXML
 
     public void login(ActionEvent actionEvent) {
 
@@ -44,26 +28,24 @@ public class Controller {
         }
     }
 
-    public int validarUsuario(){
-         String usuario = txtUsuario.getText();
-         String password = txtPassword.getText();
-        int resultado=0;
-        try {
-            PreparedStatement sentencia = Conexion.abrirConexion().prepareStatement(
-                    "SELECT * FROM usuarios WHERE nombre_usuario = ? AND palabra_clave = ?;"
-            );
-            sentencia.setString(1, usuario);
-            sentencia.setString(2, password);
-            ResultSet resultados = sentencia.executeQuery();
+    public void crearUsuario(ActionEvent actionEvent) {
 
-            while (resultados.next()) {
-                resultado=1;
-            }
-
-        } catch (SQLException e) {
+        try
+        {
+            Stage planillaStage=new Stage();
+            AnchorPane root = (AnchorPane) FXMLLoader.load(getClass().getResource("CrearUsuario/crearUsuario.fxml"));
+            Scene scene = new Scene(root,560,390);
+            planillaStage.setScene(scene);
+            planillaStage.show();
+        }
+        catch(Exception e)
+        {
             e.printStackTrace();
         }
-        return resultado ;
     }
+
+
+
+
 
 }
---
+++ src/sample/InicioSesion/ControllerInicioSesion.java
@@ -0,0 +1,86 @@
+package sample.InicioSesion;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import sample.modelos.Usuario;
+import sample.modelos.UsuarioIniciarSesion;
+
+
+public class ControllerInicioSesion {
+    @FXML
+    private  Button btnLogin;
+
+    @FXML
+    private TextField txtNombre_Usuario;
+
+    @FXML
+    private PasswordField txtPalabraClave;
+
+    private void btnLogin(){
+        Stage stage = (Stage) btnLogin.getScene().getWindow();
+        stage.close();
+    }
+
+    public UsuarioIniciarSesion crearInstancia() {
+        UsuarioIniciarSesion inicioSesion = new
+                UsuarioIniciarSesion(txtNombre_Usuario.getText(),
+                txtPalabraClave.getText());
+        return inicioSesion;
+
+    }
+
+
+    public void comprobarCampos() {
+        String usuario = txtNombre_Usuario.getText().toString();
+        String contrasena = txtPalabraClave.getText().toString();
+        mensaje(usuario);
+        mensaje(contrasena);
+        if (!txtNombre_Usuario.getText().isEmpty() &&
+                !txtPalabraClave.getText().isBlank()){
+            if (UsuarioIniciarSesion.comprobandoUsario(usuario,contrasena)) {
+                mensaje("Ha iniciado sesión correctamente");
+                try
+                {
+                    Stage planillaStage=new Stage();
+                    AnchorPane root = (AnchorPane) FXMLLoader.load(getClass().getResource("Home/Home.fxml"));
+                    Scene scene = new Scene(root,560,390);
+                    planillaStage.setScene(scene);
+                    planillaStage.show();
+                }
+                catch(Exception e)
+                {
+                    e.printStackTrace();
+                }
+            } else {
+                mensaje("Usario y/o contraseña incorrectos");
+            }
+    }else{
+            mensaje("Usario y/o contraseña vacios");
+        }
+
+    }
+
+
+    private void mensaje(String mensaje) {
+
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setTitle(null);
+        alert.setHeaderText(null);
+        alert.setContentText(mensaje);
+        alert.showAndWait();
+    }
+
+
+    public void home(ActionEvent actionEvent) {
+        comprobarCampos();
+
+    }
+}
